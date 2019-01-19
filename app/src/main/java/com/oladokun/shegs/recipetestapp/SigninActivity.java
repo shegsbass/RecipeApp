9d86da405df3eacedcea84afc3aba670325e54c8
shegsbass <oladokunoluwasegun@gmail.com>
2019-01-19 20:21:08 +0100

    Signin page implemented

--- app/src/main/java/com/oladokun/shegs/recipetestapp/SigninActivity.java
+++ app/src/main/java/com/oladokun/shegs/recipetestapp/SigninActivity.java
@@ -4,12 +4,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+public class SigninActivity extends AppCompatActivity {
 
     private TextView Button;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,37 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signin);
 
         Button = findViewById(R.id.getstarted);
-        Button.setOnClickListener(this);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        /*Button.setOnClickListener(this);*/
+
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkDataEntered();
+            }
+        });
     }
 
-    @Override
+    boolean isEmail (EditText text){
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+    boolean isEmpty (EditText text){
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered(){
+        if (isEmail(email) == false){
+            email.setError("Enter a valid email address");
+        }
+    }
+
+   /* @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
